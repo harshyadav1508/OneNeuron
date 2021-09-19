6fa56c68fbfe4e690a9e9e9be2f6235664a8a45f
harshyadav1508 <yaduvanshiharsh15@gmail.com>
2021-09-19 14:56:42 +0530

    updated docstring

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # OneNeuron
 OneNeuron | perceptron
+
+# Commands Used
+
+'''bash
+git add . && git commit -m "my_msg" && git push origin master
+'''
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -7,19 +7,38 @@ import os
 plt.style.use("fivethirtyeight") # THIS IS STYLE OF GRAPHS
 
 def prepare_data(df):
+  """it is used to separate the dependent variables and independent features
+
+  Args:
+      df (pd.DataFrame): it is pandas dataframe
+
+  Returns:
+      tuple: it returns the tuples of dependent variables and independent variables
+  """
   X = df.drop("y", axis=1)
-
   y = df["y"]
-
   return X, y
 
 def save_model(model, filename):
+  """This saves the trained model 
+
+  Args:
+      model (python object): trained model
+      filename (str): path to save the trained model
+  """
   model_dir = "models"
   os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   filePath = os.path.join(model_dir, filename) # model/filename
   joblib.dump(model, filePath)
 
 def save_plot(df, file_name, model):
+  """for saving the generated image  
+
+  Args:
+      df (pd.DataFrame): it is pandas dataframe
+      file_name (str): its path to save the plot
+      model (python object): trained model
+  """
   def _create_base_plot(df):
     df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
     plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
